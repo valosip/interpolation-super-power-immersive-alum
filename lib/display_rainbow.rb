c68197124e05866a3956a315383b09ab81299440
Val Osip <valosip@gmail.com>
2020-04-02 06:19:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(array)
+  colors = []
+  array.each do |color|
+      colors << "#{color[0].upcase}: #{color}"
+  end
+  return colors.join(", ")
+end
